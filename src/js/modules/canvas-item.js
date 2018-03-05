f50528ae639a6ce505353b30392a24750878c64e
kiullikki <kiullikki@gmail.com>
2018-03-05 19:07:58 +0300

    balla erroe

--- src/js/modules/canvas-item.js
+++ src/js/modules/canvas-item.js
@@ -20,8 +20,6 @@ export class CanvasItem {
         this.colors = colors;
         this.sizes = drawElemSizes;
         this.linesPath = [];
-        this.queqe = [];
-        // this.rendered = false;
 
         this.init();
     }
@@ -34,59 +32,11 @@ export class CanvasItem {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-
-    afterInitOther() {
-        return new Promise(resolve => {
-            let wait = setInterval(() => {
-                if(this.rendered){
-                    clearInterval(wait);
-                    resolve();
-                }
-            }, 10)
-        })
-    }
-
-    afterInitOther() {
-        returngit  Promise.resolve();
-    }
-
-
-    // drawOther(callback) {
-    //     this.canvas.width = +this.canvas.getAttribute('data-width');
-    //     this.canvas.height = +this.canvas.getAttribute('data-height');
-    //     this.drawBackground().then(() => {
-    //         this.createLines().then(() => {
-    //             this.rendered = true;
-    //             // if(callback){
-    //             //     callback();
-    //             // }
-    //         });
-    //     });
-    // }
-
     draw(){
         this.canvas.width = +this.canvas.getAttribute('data-width');
         this.canvas.height = +this.canvas.getAttribute('data-height');
         this.createLines();
         };
-
-    drawBackground() {
-        // const img = new Image();
-        // img.src = this.pathes.bg;
-        // return new Promise((resolve, reject)=>{
-        //     img.onload = () => {
-        //         this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
-        //         resolve();
-        //     };
-        // })
-
-        return Promise.resolve();
-
-    }
-
-    addToQueue(action){
-        this.queqe.push(action);
-    }
 
     createLines() {
         this.stadiums.forEach((stadium, index) => {
@@ -94,23 +44,6 @@ export class CanvasItem {
         });
 
     }
-
-    // createLinesOther() {
-    //     return new Promise((resolve, reject) => {
-    //         this.stadiums.forEach((stadium, index) => {
-    //             this.linesPath.push(new Line(stadium.coords, index, this.coordsStart, this.ctx, this.colors, this.sizes, this.pathes));
-    //             if((this.stadiums.length - 1) === index) {
-    //                 let waitResolve = setInterval(() => {
-    //                    if(this.linesPath[this.linesPath.length - 1].done){
-    //                        clearInterval(waitResolve);
-    //                        resolve();
-    //                    }
-    //                 },10)
-    //             }
-    //         });
-    //     });
-    // }
-
 
     createBall(coords) {
         new Ball(this.ctx, coords, this.sizes.ball, this.colors.ball);
